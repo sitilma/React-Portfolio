6dd25d820a967c41fd7aef4f2e6a65c215df0442
sitilma <sitilma012@gmail.com>
2018-11-21 00:54:26 +0900

    fix by reducers default lang "Readme" => "README"

--- src/reducers/lang.js
+++ src/reducers/lang.js
@@ -1,12 +1,12 @@
 import { LANG } from "../actions";
 
-const initialState = { codeName: "Readme" };
+const initialState = { codeName: "README" };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case LANG:
       return { lang: action.lang };
     default:
-      return state;
+      return { lang: state.codeName };
   }
 };
