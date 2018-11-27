2f1a2fc7c2ec3e6df3f7770d600d01f3d0963a9a
sitilma <sitilma012@gmail.com>
2018-11-27 14:13:46 +0900

    delte yarnlock && add Terminal

--- src/components/terminal/terminal.js
+++ src/components/terminal/terminal.js
@@ -3,9 +3,17 @@ import "./terminal.css";
 
 function Terminal(props) {
   return (
-    <div className={"terminal"}>
+    <div className={"terminalContainer"}>
       <div className={"terminalHeader"}>
         <p>TERMINAL</p>
+        <p>OUTPUT</p>
+        <p>DEBUG CONSOLE</p>
+      </div>
+      <div className={"terminalContent"}>
+        <p className={"terminalText"}>
+          ~/Desktop/Myportfolio<span className={"terminalGit"}>(master)</span>
+        </p>
+        <p className={"terminalInput"}>$</p>
       </div>
     </div>
   );
